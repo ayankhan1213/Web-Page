d2e294e95a2e7bfdf3dc1ae076a0ca7891aee10b
Ayan Khan <aw1800606@gmail.com>
2026-06-10 15:40:20 +0500

    Making hero section

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import "./Hero.css"
 import HeroVideo from '../../assets/hero-video.mp4'
+import Button from './Button'
 const Hero = () => {
   return (
     <>
-        <div className="video-section">
-            <video autoPlay muted loop src={HeroVideo}></video>
+      <div className="video-section">
+        <video autoPlay muted loop src={HeroVideo}></video>
+      </div>
+
+      <div className="hero-bottom">
+        <div className="heroRightPart">
+          <h2>World’s no.1 NEV brand*</h2>
+          <p>*Based on the publicly available sources</p>
         </div>
+
+        <div className="heroLeftPart">
+            <div className="duo">
+              <Button text="Express your Interest" />
+              <Button text="Book a test Drive" />
+            </div>
+            <Button text="Book now" />
+        </div>
+      </div>
     </>
   )
 }
